bd75a3d7330e96c934f17091833c9b9b0eb242fb
Joel Reyes <joel.rys98@gmail.com>
2023-02-16 13:45:35 -0600

    borders

--- FloristeriaProyecto.Android/Resources/Resource.designer.cs
+++ FloristeriaProyecto.Android/Resources/Resource.designer.cs
@@ -22509,19 +22509,22 @@ namespace FloristeriaProyecto.Droid
 			public const int preference_list_divider_material = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int shop = 2131165358;
+			public const int rosa = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int test_custom_background = 2131165359;
+			public const int shop = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int tooltip_frame_dark = 2131165360;
+			public const int test_custom_background = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int tooltip_frame_light = 2131165361;
+			public const int tooltip_frame_dark = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int xamarin_logo = 2131165362;
+			public const int tooltip_frame_light = 2131165362;
+			
+			// aapt resource value: 0x7F0700B3
+			public const int xamarin_logo = 2131165363;
 			
 			static Drawable()
 			{
