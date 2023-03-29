d1760c561a41b7f65989bdcbe1b2e6032bbd3acd
Test02 <t36t02@gmail.com>
2023-03-29 01:20:28 -0600

    Nuevos campos

--- FloristeriaProyecto/Modelo/Despacho.cs
+++ FloristeriaProyecto/Modelo/Despacho.cs
@@ -9,8 +9,9 @@ namespace FloristeriaProyecto.Modelo
         public string personaContacto { get; set; }
         public string direccion { get; set; }
         public string departamento { get; set; }
-      //  public string provincia { get; set; }
-      //  public string distrito { get; set; }
         public string celular { get; set; }
+
+        public double longitud { get; set; }
+        public double latitud { get; set; }
     }
 }
