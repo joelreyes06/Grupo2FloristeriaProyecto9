5681a44b4ba0c622f2ba6dece3ff3d7ce44219e3
Joel Reyes <joel.rys98@gmail.com>
2023-04-15 01:33:41 -0600

    icon

--- FloristeriaProyecto.Android/MainActivity.cs
+++ FloristeriaProyecto.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.CurrentActivity;
 
 namespace FloristeriaProyecto.Droid
 {
-    [Activity(Label = "FloristeriaProyecto", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FloristeriaProyecto", Icon = "@mipmap/iconstore", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
